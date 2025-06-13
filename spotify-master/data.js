3354d6c2ed501b32138a5d0147d4746fd21fdae4
mianchen1031 <162455245+mianchen1031@users.noreply.github.com>
2025-06-13 20:13:09 +0800

    Update data.js

--- spotify-master/data.js
+++ spotify-master/data.js
@@ -1,48 +1,64 @@
 const cardInfo = [
     {
+        bgColor: '255,208,213',
+        imgUrl: './asset/img/009.png',
+        tips: '2024年7月26日',
+        contnetText: '马斯克在 X 平台转发了一段伪造的哈里斯声音视频，其中 AI 生成的语音声称 “批评我就是种族歧视”。该视频截取了哈里斯竞选广告的画面，但替换了原音轨，尽管 X 平台政策禁止传播误导性媒体，但马斯克以 “言论自由” 为由拒绝删除，导致该视频浏览量突破 1.27 亿次。',
+        link: "https://x.com/elonmusk/status/1816974609637417112" 
+    },
+    {
         bgColor: '255,188,75',
         imgUrl: './asset/img/008.png',
         tips: '2024年3月23日',
-        contnetText: '亚利桑那州议会创建了参议院候选人卡里·莱克（Kari Lake）的深度伪造视频，视频台词为“欢迎来到人工智能深度伪造的可怕新时代。这才刚刚开始。2024 年的选举将是历史上第一次，任何拥有计算机的白痴都可以制作令人信服的视频来描绘具有全球重要性的虚假事件，并在几分钟内将其发布给全世界。包括我们”。'
+        contnetText: '亚利桑那州议会创建了参议院候选人卡里·莱克（Kari Lake）的深度伪造视频，视频台词为“欢迎来到人工智能深度伪造的可怕新时代。这才刚刚开始。2024 年的选举将是历史上第一次，任何拥有计算机的白痴都可以制作令人信服的视频来描绘具有全球重要性的虚假事件，并在几分钟内将其发布给全世界。包括我们”。',
+        link: "https://x.com/aisurgenet/status/1772375365085700537" 
     },
     {
         bgColor: '164,201,216',
         imgUrl: './asset/img/005.png',
         tips: 'Black Voters Matter',
-        contnetText: '鼓励黑人投票的组织“黑人选民问题”（Black Voters Matter）的联合创始人克里夫·奥尔布赖特（Cliff Albright）担心，所谓的“深度伪造”图像正在被用来影响2024年总统大选的结果。'
+        contnetText: '鼓励黑人投票的组织“黑人选民问题”（Black Voters Matter）的联合创始人克里夫·奥尔布赖特（Cliff Albright）担心，所谓的“深度伪造”图像正在被用来影响2024年总统大选的结果。',
+        link: "https://x.com/Mollyploofkins/status/1764630779563225324" 
     },
     {
         bgColor: '255,208,213',
         imgUrl: './asset/img/004.png',
         tips: '2024年3月4日',
-        contnetText: '据每日电讯报，唐纳德·特朗普的支持者正在向黑人选民传播数十个将黑人描绘成支持前总统的深度伪造图片。'
+        contnetText: '据每日电讯报，唐纳德·特朗普的支持者正在向黑人选民传播数十个将黑人描绘成支持前总统的深度伪造图片。',
+        link: "https://x.com/DigitalWarfare1/status/1764587237126148167" 
     },     {
         bgColor: '255,188,75',
         imgUrl: './asset/img/007.png',
         tips: '2024年1月30日',
-        contnetText: '网络流传的一系列照片据称显示拜登总统身着军装，即将授权进行军事打击。法新社经分析得出结论该图像是 AI 生成的。'
+        contnetText: '网络流传的一系列照片据称显示拜登总统身着军装，即将授权进行军事打击。法新社经分析得出结论该图像是 AI 生成的。',
+        link: "https://nypost.com/2024/01/30/business/ai-generated-images-of-joe-biden-in-military-uniform-go-viral-amid-middle-east-tensions/" 
     },
     {
         bgColor: '255,208,213',
         imgUrl: './asset/img/006.png',
         tips: '2023年7月8日',
-        contnetText: '特朗普和拜登参与电视辩论的人工智能生成版本正在大面积传播，其讲话内容充斥着冒犯性语言，深度伪造完成了制作脚本、语音克隆和口型同步等视频操作。'
+        contnetText: '特朗普和拜登参与电视辩论的人工智能生成版本正在大面积传播，其讲话内容充斥着冒犯性语言，深度伪造完成了制作脚本、语音克隆和口型同步等视频操作。',
+        link: "https://x.com/elonmusk/status/1816974609637417112" 
     },   {
         bgColor: '255,188,75',
         imgUrl: './asset/img/003.png',
         tips: '2023年6月26日',
-        contnetText: '多伦多市长选举中，保守派候选人Anthony Furey用深度伪造图像作为竞选材料，其中一幅图像展示了一条城市街道，街道两旁的人是在建筑物旁露营的流浪者。'
+        contnetText: '多伦多市长选举中，保守派候选人Anthony Furey用深度伪造图像作为竞选材料，其中一幅图像展示了一条城市街道，街道两旁的人是在建筑物旁露营的流浪者。',
+        link: "https://x.com/BenSpurr/status/1668355886606082081" 
     },
     {
         bgColor: '249,193,192',
         imgUrl: './asset/img/001.png',
         tips: '2023年6月6日',
-        contnetText: '佛罗里达州州长罗恩·德桑蒂斯也利用AI合成图像发布了前总统特朗普和前卫生官员安东尼·福奇博士亲密拥抱的虚假照片。'
+        contnetText: '佛罗里达州州长罗恩·德桑蒂斯也利用AI合成图像发布了前总统特朗普和前卫生官员安东尼·福奇博士亲密拥抱的虚假照片。',
+        link: "https://x.com/DeSantisWarRoom/status/1665799058303188992" 
     },
     {
         bgColor: '85,171,237',
         imgUrl: './asset/img/002.png',
         tips: '2023年4月25日',
-        contnetText: '美国共和党全国委员会在总统拜登宣布竞选连任后公布了一段AI视频，描绘了假如拜登连任后的世界末日景象；'
+        contnetText: '美国共和党全国委员会在总统拜登宣布竞选连任后公布了一段AI视频，描绘了假如拜登连任后的世界末日景象；',
+        link: "https://x.com/elonmusk/status/1816974609637417112" 
     },
+    
 ]
